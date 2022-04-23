1a64de8a9ea143672bb1fa6cac7e75d7c1cbb9fb
Hasibul Hasan <hasibulhasan442@gmail.com>
2022-04-23 21:56:43 +0600

    Set Navbar

--- katalyst/src/App.js
+++ katalyst/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./Component/NavBar/Navbar";
+import Aboutus from "./Pages/Aboutus/Aboutus";
+import Homepage from "./Pages/HomePage/Homepage";
+import Product from "./Pages/Products/Product";
+import Partners from "./Pages/Partners/Partners";
+import ContactUs from "./Pages/Contactus/ContactUs";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/home" element={<Homepage />} />
+          <Route path="/about-us" element={<Aboutus />} />
+          <Route path="/basic-chemicals" element={<Product/>} />
+          <Route path="/auxiliary-chemicals" element={<Product/>} />
+          <Route path="/dyes-stuff" element={<Product/>} />
+          <Route path="/machines" element={<Product/>} />
+          <Route path="/partners" element={<Partners/>} />
+          <Route path="/contact-us" element={<ContactUs/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ katalyst/src/Component/NavBar/Navbar.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <Link className="navbar-brand" to='/'>
+            <img src="/logo/katalyst.png" alt="Company Logo" height= '50px' className="navbar-logo"/>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/home">
+                  Home
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/about-us">
+                  About Us
+                </Link>
+              </li>
+
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  to="/"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Products
+                </Link>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <Link className="dropdown-item" to="/basic-chemicals">
+                      Basic Chemicals
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/auxiliary-chemicals">
+                      Auxiliary Chemicals
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/dyes-stuff">
+                      Dyes Stuff
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/machines">
+                      Machines
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/partners">
+                  Partners
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact-us">
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
