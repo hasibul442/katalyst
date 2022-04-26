3334def0ab357df2a9906e1e20d7b7119c56d123
Mohammad Hasibul Hasan <hasibulhasan442@gmail.com>
2022-04-26 15:51:31 +0600

    Footer Change

--- katalyst/src/Component/Footer/Footer.jsx
+++ katalyst/src/Component/Footer/Footer.jsx
@@ -5,12 +5,22 @@ function Footer() {
   return (
     <>
       <section>
-        <div className="footer2">
-          <p className="text-center pt-2 pb-2">
+        <div className="footer">
+          <img src="/footer/Untitled-1.png" className="img-fluid" style={{ height: '150px', width: '100%' }}/>
+          <div class="centered">
+          <p className="text-center footer-text pt-2 pb-2">
             Copyright © {new Date().getFullYear()} Katalyst Bangladesh | All Rights
             Reserved
           </p>
+          </div>
         </div>
+        {/* <div className="footer2">
+          <p className="text-center footer-text pt-2 pb-2">
+            Copyright © {new Date().getFullYear()} Katalyst Bangladesh | All Rights
+            Reserved
+          </p>
+
+        </div> */}
       </section>
     </>
   );
