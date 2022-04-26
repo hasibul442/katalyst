89b0645db9b54eaca8cdc4ef899720a91c0e9baf
Mohammad Hasibul Hasan <hasibulhasan442@gmail.com>
2022-04-26 16:34:24 +0600

    Footer Change

--- katalyst/src/Component/Footer/Footer.jsx
+++ katalyst/src/Component/Footer/Footer.jsx
@@ -7,7 +7,18 @@ function Footer() {
       <section>
         <div className="footer">
           <img src="/footer/Untitled-1.png" className="img-fluid" style={{ height: '150px', width: '100%' }}/>
-          <div class="centered">
+          <div className="centered">
+          <div>
+            <div className="social-container">
+              <ul className="social-icons">
+                  <li><a href="#"><i className="fab fa-facebook"></i></a></li>
+                  <li><a href="#"><i className="fab fa-instagram"></i></a></li>
+                  <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+                  <li><a href="#"><i className="fab fa-linkedin"></i></a></li>
+                  <li><a href="#"><i className="fab fa-codepen"></i></a></li>
+              </ul>
+            </div>
+          </div>
           <p className="text-center footer-text pt-2 pb-2">
             Copyright © {new Date().getFullYear()} Katalyst Bangladesh | All Rights
             Reserved
