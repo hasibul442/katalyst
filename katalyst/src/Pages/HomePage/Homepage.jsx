21c789ad5be3e5c2086ed4b4fb8ef37a5b20f9e7
Hasibul Hasan <hasibulhasan442@gmail.com>
2022-05-17 00:34:04 +0600

    Homepage Design

--- katalyst/src/Pages/HomePage/Homepage.jsx
+++ katalyst/src/Pages/HomePage/Homepage.jsx
@@ -24,9 +24,25 @@ function Homepage() {
         </CCarousel>
       </section>
 
-      <div className="custome-shape">
+      <section className="custome-shape">
         <img src="/contact/11.png" className="shape-image" alt="Custom Shape" />
-      </div>
+      </section>
+
+      <section className='pt-5'>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6">
+                <div className='history-img text-center'>
+                  <img src="/homepage/homepage-1.jpg" alt="" className='img-fluid history-side-image' />
+                </div>
+              </div>
+              <div className="col-md-6">
+                <p className='h2 katalyst-history-headline'>Katalyst Bangladesh, is a partnership entity of four professionals having academic background of Textile Engineering and Biochemistry with combined pharmaceutical and textile experiences of more than 75 years</p>
+                <p>has been established in early 2019 considering the growth potentiality of textile dyeing and washing industries. And, to exclusively focus in its target industries currently Katalyst Bangladesh is closely working with one of the major sole distributor of soda ash light and Soda Ash Dense from Double Ring, China. Besides, Katalyst Bangladesh sources others basic chemicals from renowned manufacturer worldwide which include Glauber Salt from Sateri (China), </p>
+              </div>
+            </div>
+          </div>
+      </section>
     </>
   )
 }
